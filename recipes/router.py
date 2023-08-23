cbec3ab46a375f2b76dcc7515707daef00507629
Sam Thorold <sam.thorold@gmail.com>
2023-08-23 11:22:34 +0100

    Basic HTTP statuses on exception

--- recipes/router.py
+++ recipes/router.py
@@ -1,6 +1,6 @@
 import logging
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 
 from recipes import domain
 from recipes import services
@@ -12,16 +12,25 @@ router = APIRouter()
 
 @router.get("/recipes/")
 async def get_recipes() -> list[domain.RecipeInDB]:
-    return await services.Services().get_recipes()
+    try:
+        return await services.Services().get_recipes()
+    except Exception as e:
+        raise HTTPException(500, "Could not get recipes") from e
 
 
 @router.post("/recipes/", status_code=201)
 async def create_recipe(recipe: domain.Recipe) -> domain.RecipeInDB:
     logger.info("recipe=%r", recipe)
-    return await services.Services().create_recipe(recipe=recipe)
+    try:
+        return await services.Services().create_recipe(recipe=recipe)
+    except Exception as e:
+        raise HTTPException(500, "Could not create recipe") from e
 
 
 @router.get("/recipes/{recipe_id}")
 async def get_recipe(recipe_id: str) -> domain.RecipeInDB:
     logger.info("recipe_id=%s", recipe_id)
-    return await services.Services().get_recipe(recipe_id=recipe_id)
+    try:
+        return await services.Services().get_recipe(recipe_id=recipe_id)
+    except Exception as e:
+        raise HTTPException(500, "Could not get recipe") from e
